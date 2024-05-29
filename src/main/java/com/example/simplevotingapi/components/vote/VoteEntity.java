7126df0a51775aa573196405ad7f5612e3fb1a0a
Soromeister <sorin.m.oprea+github@gmail.com>
2024-05-29 15:43:31 +0200

    Really embed the class for simplicity.

--- src/main/java/com/example/simplevotingapi/components/vote/VoteEntity.java
+++ src/main/java/com/example/simplevotingapi/components/vote/VoteEntity.java
@@ -1,17 +1,23 @@
 package com.example.simplevotingapi.components.vote;
 
+import com.example.simplevotingapi.components.candidate.CandidateEntity;
+import com.example.simplevotingapi.components.user.UserEntity;
 import com.example.simplevotingapi.components.vote.output.Vote;
 import io.github.linpeilie.annotations.AutoMapper;
 import io.github.linpeilie.annotations.AutoMapping;
 import io.github.linpeilie.annotations.AutoMappings;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,6 +31,28 @@ import lombok.Setter;
 @Table(name = "Votes")
 @AutoMapper(target = Vote.class, cycleAvoiding = true)
 public class VoteEntity implements Serializable {
+
+    @Getter
+    @Setter
+    @Builder
+    @Embeddable
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class VoteEntityId implements Serializable {
+
+        @ManyToOne(optional = false)
+        @JoinColumn(name = "user_id", nullable = false)
+        private UserEntity user;
+
+        @ManyToOne(optional = false)
+        @JoinColumn(name = "candidate_id", nullable = false)
+        private CandidateEntity candidate;
+
+        @ManyToOne(optional = false)
+        @JoinColumn(name = "round_id", nullable = false)
+        private VoteRoundEntity votingRound;
+    }
 
     @EmbeddedId
     @AutoMappings({@AutoMapping(source = "id.user", target = "user"),
