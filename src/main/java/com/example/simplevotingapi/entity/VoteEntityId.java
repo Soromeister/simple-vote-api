a18a922b47c7eb2ad8733749d4bf90e67cd6eea2
Soromeister <sorin.m.oprea+github@gmail.com>
2024-05-29 15:02:35 +0200

    Remove EqualsAndHashCode Remove MainController

--- src/main/java/com/example/simplevotingapi/entity/VoteEntityId.java
+++ src/main/java/com/example/simplevotingapi/entity/VoteEntityId.java
@@ -6,6 +6,7 @@ import jakarta.persistence.ManyToOne;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Setter
 @Builder
 @Embeddable
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class VoteEntityId implements Serializable {
